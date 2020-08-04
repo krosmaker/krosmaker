14ad12eb27007ef385f1d98991aa4da1f31182d9
MJ <michal@jadczuk.pl>
2020-08-05 00:04:37 +0200

    (#1) Project setup.

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title class="toolbar-title" v-text="'Krosmaker'" />
+    </v-app-bar>
+    <v-main>
+      <v-container fill-height fill-width fluid>
+        <nuxt />
+      </v-container>
+    </v-main>
+<v-footer
+    class="main-footer"
+    dark
+    height="auto"
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-actions class="grey darken-3 justify-center">
+        <span>
+          The presented graphics from <strong>Krosmaster: Blast</strong> are
+          copyrighted by <strong>Ankama</strong>. This website is not produced,
+          endorsed, or affiliated with <strong>Ankama</strong>.
+        </span>
+      </v-card-actions>
+
+      <v-card-actions class="grey darken-4 justify-center">
+        <span>&copy;2020 — <strong>MJ</strong></span>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+  </v-app>
+</template>
+
+<style scoped lang="scss">
+.toolbar-title {
+  font-weight: 900;
+}
+
+.main-footer {
+  padding: 0px;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,81 @@
+import colors from "vuetify/es5/util/colors";
+
+export default {
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "universal",
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "static",
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    titleTemplate: "%s - Krosmaker",
+    title: "Krosmaster: Blast Card Creator",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "A web-based tool that allows to create custom Krosmaster: Blast cards.",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxt/typescript-build", "@nuxtjs/vuetify"],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ["~/assets/style/variables.scss"],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
+};
