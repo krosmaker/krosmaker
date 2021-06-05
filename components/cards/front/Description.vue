a5ccbdf33773d8e18275e229fffe930791b25be0
MJ <michal@jadczuk.pl>
2021-06-05 17:01:26 +0200

    (#32) Marker symbols padding.

--- components/cards/front/Description.vue
+++ components/cards/front/Description.vue
@@ -119,7 +119,7 @@ export default class Description extends Vue {
   width: 19px;
   height: 19px;
   margin-top: -5px;
-  margin-left: -2px;
+  margin-left: -1px;
   margin-right: -2px;
 }
 
@@ -143,7 +143,6 @@ export default class Description extends Vue {
   width: 19px;
   height: 19px;
   margin-top: -5px;
-  margin-left: -1px;
   margin-right: -1px;
 }
 
